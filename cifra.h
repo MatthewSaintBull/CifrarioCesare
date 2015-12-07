de447f97b9bf6cdcf082d2c036c71cce18bd24cb
megainter97 <mattinter997@gmail.com>
2015-12-07 19:55:25 +0100

    Aggiunto cifra.h

---
+++ cifra.h
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+using namespace std;
+void cifra_decifra(string testocif,int chiave,bool com){
+	int n=testocif.size();
+	char p[n];
+	strcpy(p,testocif.c_str());
+	if(com==true){
+		for(int i=0;i<n;i++)
+			p[i]=p[i]+chiave;
+	}
+	else{
+		for(int i=0;i<n;i++)
+                p[i]=p[i]-chiave;
+	}
+	string testo(p);
+	cout << testo<<endl;
+
+}
